add48ef89b45b26ee14ec5c719923706924d7408
smokie <smokie112000@gmail.com>
2025-04-20 20:23:46 +0600

    add svelte, mdx support, remove depreciated experimental feature

--- astro.config.ts
+++ astro.config.ts
@@ -5,14 +5,16 @@ import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import { SITE } from "./src/config";
 
+import mdx from "@astrojs/mdx";
+
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
-  integrations: [
-    sitemap({
-      filter: page => SITE.showArchives || !page.endsWith("/archives"),
-    }),
-  ],
+  integrations: [sitemap({
+    filter: page => SITE.showArchives || !page.endsWith("/archives"),
+  }), mdx(), svelte()],
   markdown: {
     remarkPlugins: [remarkToc, [remarkCollapse, { test: "Table of contents" }]],
     shikiConfig: {
